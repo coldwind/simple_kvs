51b1ea079f12601da6660c177609c1867af37287
vegeta.ma@163.com <root@localhost.localdomain>
2016-10-08 04:04:46 -0700

    network

--- networking.c
+++ networking.c
@@ -4,6 +4,7 @@ int knet_tcp_init(const char *ip, uint32_t port, int backlog) {
     int sock_fd, bind_res, listen_res;
 
     struct sockaddr_in serv_addr;
+    bzero(serv_addr, sizeof(serv_addr));
 
     serv_addr.sin_family = AF_INET;
 
@@ -30,12 +31,34 @@ int knet_tcp_init(const char *ip, uint32_t port, int backlog) {
     return sock_fd;
 }
 
-int kvs_tcp_accept(int sock_fd) {
-    int cli_fd = accept(sock_fd, (struct sockaddr*)&cli_addr, sizeof(cli_addr));
+int epoll_run(int sock_fd) {
 
-    return cli_fd;
+    int epoll_fd, nfds, connfd;
+    struct sockaddr_in cli_addr;
+    struct epoll_event ep_event, ep_events[EPOLL_CREATE_SIZE];
+    socklen_t socklen = sizeof(cli_addr);
+
+    ep_event.events = EPOLLIN | EPOLLET;
+    ep_event.data.fd = sock_fd;
+
+    epoll_fd = epoll_create(EPOLL_CREATE_SIZE);
+    epoll_ctl(epoll_fd, EPOLL_CTL_ADD, sock_fd, &ep_event);
+
+    for (;;) {
+        nfds = epoll_wait(epoll_fd, &ep_events, EPOLL_CREATE_SIZE, -1);
+        for (int i = 0; i < nfds; i++) {
+            if (ep_events[i].data.fd == sock_fd) {
+                ep_event.events = EPOLLIN | EPOLLET;
+                ep_event.data.fd = connfd;
+                connfd = accept(sock_fd, (struct sockaddr *)&cli_addr, &socklen);
+                epoll_ctl(epoll_fd, EPOLL_CTL_ADD, connfd, &ep_event);
+            } else if (ep_events[i].event & EPOLLIN) {
+                // 读数据
+            }
+        }
+    }
+
+    return epoll_fd;
 }
 
-int iomodel() {
-    epoll_create();
-}
+
--- networking.h
+++ networking.h
@@ -1,3 +1,5 @@
 struct sockaddr_in cli_addr;
 
 int knet_tcp_init(const char*, uint32_t, int);
+int epoll_run(int);
+void read_data(int fd);
